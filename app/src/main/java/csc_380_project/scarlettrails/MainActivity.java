7ff1e30434ca79e6021e2002ec6b822f4083fc64
Nathan Stemmler <nstemmle@oswego.edu>
2014-10-10 00:26:41 -0400

    Commit Friday 10/10/2014 12:25 AM - Google Maps Works!

--- app/src/main/java/csc_380_project/scarlettrails/MainActivity.java
+++ app/src/main/java/csc_380_project/scarlettrails/MainActivity.java
@@ -6,13 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TestActivity extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-        int test;
+        setContentView(R.layout.activity_main);
     }
 
     @Override
