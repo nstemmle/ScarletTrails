9bff9faca7e19895a4c9dab2738c5b715f1d1eb8
smillspa <smillspa@users.noreply.github.com>
2014-10-26 21:23:10 -0400

    Update Forecast.java

--- app/src/main/java/csc_380_project/scarlettrails/Forecast.java
+++ app/src/main/java/csc_380_project/scarlettrails/Forecast.java
@@ -1,7 +1,54 @@
 package csc_380_project.scarlettrails;
 
+import android.text.format.Time;
+
+import java.util.Date;
+
 /**
- * Created by Nathan on 10/16/2014.
+ * Created by Kheiyasa on 10/23/2014.
  */
-public class Forecast {
+public class Forecast implements DatabaseInterface {
+
+    private Date date;
+    private double tempMin;
+    private double tempMax;
+    private Time sunset;
+    private Time sunrise;
+    private int precip;
+
+    public Forecast(Date date, double tempMin, double tempMax, Time sunrise, Time sunset, int precip){
+        this.date = date;
+        this.tempMin = tempMin;
+        this.tempMax = tempMax;
+        this.sunset = sunset;
+        this.sunrise = sunrise;
+        this.precip = precip;
+    }
+
+    public Double getTempMax(){return tempMax;}
+
+    public Double getTempMin(){return tempMin;}
+
+    public int getPrecipitation(){return precip;}
+
+    public Time getSunrise(){return sunrise;}
+
+    public Time getSunset(){return sunset;}
+
+    @Override
+    public void query(String lookup) {
+
+    }
+
+    @Override
+    public void updateData() {
+
+    }
+
+    @Override
+    public String insertData() {
+        return null;
+    }
+
+
 }
