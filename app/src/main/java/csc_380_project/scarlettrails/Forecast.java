a145b711f703a18b2443ee31327ede749a15beec
smillspa <smillspa@users.noreply.github.com>
2014-11-01 21:39:50 -0400

    Update Forecast.java

--- app/src/main/java/csc_380_project/scarlettrails/Forecast.java
+++ app/src/main/java/csc_380_project/scarlettrails/Forecast.java
@@ -9,6 +9,7 @@ import java.util.Date;
  */
 public class Forecast implements DatabaseInterface {
 
+   //swiping on forecast screen for 10 day?
     private Date date;
     private double tempMin;
     private double tempMax;
@@ -16,14 +17,36 @@ public class Forecast implements DatabaseInterface {
     private Time sunrise;
     private int precip;
 
-    public Forecast(Date date, double tempMin, double tempMax, Time sunrise, Time sunset, int precip){
+    private int humidity;
+    private int windspeed;
+    private double tempCurrent;
+    private int clouds;
+
+    //single day constructor
+    public Forecast(Date date, double tempMin, double tempMax, Time sunrise, Time sunset, int precip, int clouds, int humidity, double tempCurrent, int windspeed){
         this.date = date;
         this.tempMin = tempMin;
         this.tempMax = tempMax;
         this.sunset = sunset;
         this.sunrise = sunrise;
         this.precip = precip;
+        this.clouds = clouds;
+        this.humidity = humidity;
+        this.tempCurrent = tempCurrent;
+        this.windspeed = windspeed;
+
     }
+
+    //five day constructor
+    public Forecast(Date date, double tempMin, double tempMax, int precip, int clouds){
+        this.date = date;
+        this.tempMin = tempMin;
+        this.tempMax = tempMax;
+        this.precip = precip;
+        this.clouds = clouds;
+    }
+
+    public Date getDate(){return date;}
 
     public Double getTempMax(){return tempMax;}
 
@@ -34,6 +57,14 @@ public class Forecast implements DatabaseInterface {
     public Time getSunrise(){return sunrise;}
 
     public Time getSunset(){return sunset;}
+
+    public int getHumidity(){return humidity;}
+
+    public int getWindspeed(){return windspeed;}
+
+    public double getTempCurrent(){return tempCurrent;}
+
+    public int getClouds(){return clouds;}
 
     @Override
     public void query(String lookup) {
